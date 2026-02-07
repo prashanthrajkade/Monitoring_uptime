6ecd5f9bfa6904b7c6f43284053e92ce467f6560
prashanthrajkade <prashanthrajkade@email.com>
2026-02-07 22:42:08 +0530

    Initial commit with Uptime-Kuma and webhook

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = 3001;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Uptime-Kuma test app is running 🚀');
+  res.end('Uptime-Kuma test app is running ');
 });
 
 server.listen(PORT, () => {
