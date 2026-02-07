d42044b20e061a12ad6b0236229702588f4df78c
prashanthrajkade <prashanthrajkade@email.com>
2026-02-07 18:46:24 +0530

    Initial uptime-kuma app

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const PORT = 3001;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Uptime-Kuma test app is running 🚀');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
